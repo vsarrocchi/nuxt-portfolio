07e70c9ae87d71ba24b6f3cd64d1bab12cf72823
Valesca Sarrocchi <valescasarrocchi@hotmail.com>
2020-06-23 19:36:34 +0200

    google maps fix

--- plugins/vue2-google-maps.js
+++ plugins/vue2-google-maps.js
@@ -1,7 +1,6 @@
 import Vue from 'vue'
-
 import * as VueGoogleMaps from '@/node_modules/vue2-google-maps'
 
 Vue.use(VueGoogleMaps, {
-  load: { key: process.env.VUE_APP_GOOGLE_MAPS_API_KEY }
+  load: { key: 'AIzaSyAxKKo6pVqmJtEuL1R7uiuiZpisbv2GDCI' }
 })
